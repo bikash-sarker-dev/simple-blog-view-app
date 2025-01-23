3f3ebf488ecdd58f131f8b5050134ae476378dc9
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-01-23 09:26:03 +0600

    the navbar part completed

---
+++ src/app/details/layout.js
@@ -0,0 +1,5 @@
+const layoutDetails = () => {
+  return <div>layoutDetails</div>;
+};
+
+export default layoutDetails;
---
+++ src/app/profile/page.jsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+  return (
+    <div>
+      <p>Welcome to your profile.</p>
+    </div>
+  );
+};
+
+export default page;
