e17c4680de9daec5c61b96123c1972b68f586e18
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-01-24 12:06:54 +0600

    profile

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,12 +1,4 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
-
 const ProfilePage = async () => {
-  let { getUser } = getKindeServerSession();
-  const user = await getUser();
-  if (!user) {
-    redirect("/api/auth/login");
-  }
   return (
     <div className="flex justify-center items-center min-h-screen">
       <p className="bg-info text-5xl p-10 rounded-2xl">
